e98ecbc0ca4027920ff2eefe649a051b73915241
Woojoong Kim <woojoong.m.kim@gmail.com>
2022-03-08 11:27:34 +0900

    Fix linters

--- test/utils/defs.go
+++ test/utils/defs.go
@@ -73,9 +73,9 @@ func GetSliceUpdatedWeights(index int) int {
 }
 
 func GetDrbID(index int) int {
-	return (index + 5)%200
+	return (index + 5) % 200
 }
 
 func GetQci(index int) int {
-	return (index + 9)%200
+	return (index + 9) % 200
 }
